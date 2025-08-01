c15c3741eb216a03a1fa3fd5b6baa4188ca54feb
Thathsarani <hasarekat@gmail.com>
2025-08-01 23:37:00 +0530

    Added favicon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 px-20 py-4 bg-gray-800 text-white flex justify-between shadow-md">
-      <div className="text-xl font-bold">Thathsarani</div>
+      <a href="#hero" className="text-xl font-bold">Thathsarani</a>
       <div className="space-x-4">
         <a href="#hero" className="hover:text-blue-400">Home</a>
         <a href="#about" className="hover:text-blue-400">About</a>
